4da57a34ac36f7f60768b0ea461edc75c5d87588
mpapenbr <41362745+mpapenbr@users.noreply.github.com>
2021-05-02 11:25:58 +0200

    mpapenbr/issue3 (#4)
    * v1.0.5
    
    * v1.0.7
    
    * Release 1.0.7
    Fixes #3
    
    Co-authored-by: mpapenbr <markus.papenbrock@gmail.com>

--- src/check-neo.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import { readManifestsFromFile } from "./backend";
-import { bulkProcessFile } from "./stints/bulkProcessingBackend";
-import { defaultProcessRaceStateData } from "./stints/types";
-
-const manifests = readManifestsFromFile("manifest-neo.json");
-bulkProcessFile({ ...defaultProcessRaceStateData }, manifests, "data-neo.json").then((res) => {
-  fs.writeFile("neo-result.json", JSON.stringify(res), (err) => {});
-});
--- src/stints/bulkProcessing.ts
+++ src/stints/bulkProcessing.ts
@@ -1,11 +1,7 @@
 import { BulkProcessor } from "./bulkProcessor";
 import { IManifests, IProcessRaceStateData } from "./types";
 
-export const bulkProcess = (
-  current: IProcessRaceStateData,
-  manifests: IManifests,
-  jsonItems: any[]
-): IProcessRaceStateData => {
+export const bulkProcess = (manifests: IManifests, jsonItems: any[]): IProcessRaceStateData => {
   const processor = new BulkProcessor(manifests);
-  return processor.process(current, jsonItems);
+  return processor.process(jsonItems);
 };
